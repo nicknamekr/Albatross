b78c19135cdcd610b5ff9e2b0640c68794d63802
Nickname <47962021+nicknamekr@users.noreply.github.com>
2023-03-26 03:53:53 +0000

    v0.3

--- main.py
+++ main.py
@@ -10,18 +10,27 @@ def main():
 
 @app.route('/search.w')
 def search():
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt= request.args.get('q'),
-    temperature=0.65,
-    max_tokens=500,
-    stop=[" Human:", " AI:"]
+  response= openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[{"role": "user", "content": request.args.get('q')}]
   )
-  return render_template('search.html', genie_ai_body = response.choices[0].text)
+  # response = openai.Completion.create(
+  #   model="text-davinci-003",
+  #   prompt= request.args.get('q'),
+  #   temperature=0.65,
+  #   max_tokens=500,
+  #   stop=[" Human:", " AI:"]
+  # )
+  print(response)
+  return render_template('search.html', genie_ai_body = response.choices[0].message.content)
 
 @app.route('/chat.shorten')
 def chat_linking():
   return redirect('https://albatross-ai.vercel.app/', code = '302')
+
+@app.route('/ping.uptimerobot')
+def ping_for_uptimerobot():
+  return '.'
 
 @app.route('/logo.svg')
 def logo():
